27b1349e4cab100ba02979063fdaa943b55ebbbd
Henry Senior <hps1945@live.co.uk>
2017-03-04 12:56:41 +0000

    Read to merge with master

--- Stockport-Hackathon/classes/Activity.cs
+++ Stockport-Hackathon/classes/Activity.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Bing.Maps;
+
 namespace Stockport_Hackathon.classes
 {
+    public enum weatherList { SUN, WIND, RAIN, STORM}
+    public enum durationList { DAILY, WEEKLY, FORTNIGHTLY, MONTHLY, BI_MONTHLY, TERM_TIME, ANUAL, NOT_STATED, OTHER}
+
     class Activity
     {
         private string name;
@@ -15,11 +20,11 @@ namespace Stockport_Hackathon.classes
             set { name = value; }
         }
 
-        private string postcode;
-        public string Postcode
+        private Location location;
+        public Location Location
         {
-            get { return postcode; }
-            set { postcode = value; }
+            get { return location; }
+            set { location = value; }
         }
 
         private DateTime dateAndTime;
@@ -28,5 +33,22 @@ namespace Stockport_Hackathon.classes
             get { return dateAndTime; }
             set { dateAndTime = value; }
         }
+
+        private weatherList weather;
+        public weatherList Weather
+        {
+            get { return weather; }
+            set { weather = value; }
+        }
+
+        //If Activities have ranking based on popularity
+        private int rank;
+        public int Rank
+        {
+            get { return rank; }
+            set { rank = value; }
+        }
+
+
     }
 }
