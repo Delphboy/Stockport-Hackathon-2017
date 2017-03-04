681c035d65ef3b165ca0da435a494562593fcead
Rilliswen <bilyana.neshkova@gmail.com>
2017-03-04 11:02:11 +0000

    Weather Class

--- Stockport-Hackathon/MainPage.xaml.cs
+++ Stockport-Hackathon/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using APIXULib;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +31,21 @@ namespace Stockport_Hackathon
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string key = "1ffd6b91d1f14dbe93394522170403";
+            IRepository repo = new Repository();
+            var GetCityForecastWeatherResult = repo.GetWeatherData(key, GetBy.CityName, "paris", Days.Three);
+            var GetByLatLongForecastWeatherResult = repo.GetWeatherDataByLatLong(key, "30.2669444", "-97.7427778", Days.Three);
+            var GetByIPForecastWeatherResult = repo.GetWeatherDataByAutoIP(key, Days.Three);
+
+
+            var GetCityCurrentWeatherResult = repo.GetWeatherData(key, GetBy.CityName, "paris");
+            var GetByLatLongCurrentWeatherResult = repo.GetWeatherDataByLatLong(key, "30.2669444", "-97.7427778");
+            var GetByIPCurrentWeatherResult = repo.GetWeatherDataByAutoIP(key);
+
         }
     }
 }
