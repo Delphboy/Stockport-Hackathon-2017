77951865915df9f3dfb504ec4605bfbeea624478
Henry Senior <hps1945@live.co.uk>
2017-03-04 11:39:33 +0000

    Added Activity class to store data on activities

---
+++ Stockport-Hackathon/classes/Activity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockport_Hackathon.classes
+{
+    class Activity
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string postcode;
+        public string Postcode
+        {
+            get { return postcode; }
+            set { postcode = value; }
+        }
+
+        private DateTime dateAndTime;
+        public DateTime DateAndTime
+        {
+            get { return dateAndTime; }
+            set { dateAndTime = value; }
+        }
+    }
+}
--- Stockport-Hackathon/classes/MapClass.cs
+++ Stockport-Hackathon/classes/MapClass.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bing.Maps;
+
 namespace Stockport_Hackathon.classes
 {
     class MapClass
     {
         private Map m = new Map();
-
 
         public MapClass()
         {
@@ -19,6 +19,11 @@ namespace Stockport_Hackathon.classes
         public void loadAtStockport()
         {
             m.Center = new Location(53.41, -2.16);
+        }
+
+        public void plotLocation()
+        {
+
         }
     }
 }
