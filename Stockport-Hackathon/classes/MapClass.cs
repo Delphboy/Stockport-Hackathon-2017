961f6287b2f0f46d4a7474572f3ad0492cf3800c
Henry Senior <hps1945@live.co.uk>
2017-03-04 10:36:47 +0000

    Added Bing.Maps namespace reference. Started MapClass

---
+++ Stockport-Hackathon/classes/MapClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using 
+
+namespace Stockport_Hackathon.classes
+{
+    class MapClass
+    {
+        private Map mc = new Map();
+
+        public MapClass()
+        {
+
+        }
+
+        public void loadAtStockport()
+        {
+
+        }
+    }
+}
